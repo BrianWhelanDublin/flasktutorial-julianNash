9735596c98e060bbeee62a3519624402fe69b246
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-01-04 12:55:59 +0000

    add basic app functions

---
+++ run.py
@@ -0,0 +1,10 @@
+import os
+from app import app
+if os.path.exists("env.py"):
+    import env
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
